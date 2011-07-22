0d320023e68d3b54678127031602be57a4910c4c
spence <spencerwater@gmail.com>
2011-07-22 13:03:04 +0100

    first commit

---
+++ westport/content_script.js
@@ -0,0 +1,8 @@
+// Object to hold information about the current page
+var pageInfo = {
+    "title": document.title,
+    "url": window.location.href
+};
+
+// Send the information back to the extension
+chrome.extension.sendRequest(pageInfo);
