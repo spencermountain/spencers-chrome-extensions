b5580faeeffc526b4e3f7ca962924b63276bf864
spence <spencerwater@gmail.com>
2011-10-03 18:17:39 +0100

    better keep

--- thekeep/encode.js
+++ thekeep/encode.js
@@ -5,16 +5,12 @@
  */
 var mqlkey_start = 'A-Za-z0-9';
 var mqlkey_char = 'A-Za-z0-9_-';
-
 var MQLKEY_VALID = new RegExp('^[' + mqlkey_start + '][' + mqlkey_char + ']*$');
-
 var MQLKEY_CHAR_MUSTQUOTE = new RegExp('([^' + mqlkey_char + '])', 'g');
-
 
 var mqlkey_quote = function (s) {
     if (MQLKEY_VALID.exec(s))   // fastpath
         return s;
-
     var convert = function(a, b) {
         var hex = b.charCodeAt(0).toString(16).toUpperCase();
         if (hex.length == 2)
@@ -23,13 +19,10 @@ var mqlkey_quote = function (s) {
             hex = '0' + hex;
         return '$' + hex;
     };
-
     x = s.replace(MQLKEY_CHAR_MUSTQUOTE, convert);
-
     if (x.charAt(0) == '-' || x.charAt(0) == '_') {
         x = convert(x,x.charAt(0)) + x.substr(1);
     }
-
     return x;
 }
 
--- thekeep/history.js
+++ thekeep/history.js
@@ -1,42 +1,74 @@
 
 $(document).ready(function(){
-       
 
-       //history
-          d = new Date();
-          today=d.getTime();
-          day=d.getDate();
-          yesterday=d.setDate(day-1);
-          chrome.history.search({text:'', startTime:yesterday, endTime:today}, function(tabs){
-          var tablist={};
-            for(var i in tabs){
-               if(tabs[i].url.match(/^chrome/)){continue;}
-               var title=parsetitle(tabs[i].title, tabs[i].url);
-            	 var parsed=parseUri(tabs[i].url);
-	             var favicon=null;
-	             if(parsed.host){
-	               favicon='chrome://favicon/http://'+parsed.host;
-	             }
-	             domain=parsed.host.replace(/^www\./,'');
-	             domain=domain.replace(/\.(org|net|com|co\.uk)/,'')
-	             if(!title){title=domain;}
-	             if(tablist[parsed.host] == null){tablist[parsed.host]={favicon:favicon, sites:[], domain:domain };}	             
-	             tablist[parsed.host].sites.push({ url : tabs[i].url, title : title})	      
-            }   
-            //pivot into an array
-            var tabarr=[];
-            for(var i in tablist){
-              tablist[i].font=tablist[i].sites.length+15;
-              tabarr.push(tablist[i]);          
-            }
-          //  tabarr.sort(function(a,b){return b.font-a.font;});
-            
-            $('#historylist').html('');
-            $('#historytemplate').mustache({tabs:tabarr}).appendTo('#historylist');
-                   
-                   $(".domain").click(function(){
-                        $(this).siblings(".sites").toggle();
-                   });
+//thisweek date
+d = new Date();
+today=d.getTime(); 
+d.setDate(d.getDate()-7);           
+var hours=d.getHours();
+if(hours<5){      //make the day start at 5am  
+    d.setDate(d.getDate()-1);    
+}
+d.setHours(5);
+var lastweek=d.getTime();      
+console.log('last week = '+lastweek);
+
+          
+          chrome.history.search({text:'', startTime:lastweek, endTime:today}, function(tabs){
+              var tablist={};
+              for(var i in tabs){
+                 if(tabs[i].url.match(/^chrome/)){continue;}
+                 var title=parsetitle(tabs[i].title, tabs[i].url);
+              	 var parsed=parseUri(tabs[i].url);
+	               var favicon=null;
+	               if(parsed.host){
+	                 favicon='chrome://favicon/http://'+parsed.host;
+	               }
+	               domain=parsed.host.replace(/^www\./,'');
+	               domain=domain.replace(/\.(org|net|com|co\.uk)/,'')
+	               if(!title){title=domain;}
+	               if(tablist[parsed.host] == null){tablist[parsed.host]={favicon:favicon, sites:[], domain:domain };}	             
+	               tablist[parsed.host].sites.push({ url : tabs[i].url, title : title})	      
+              }   
+           
+              //pivot into an array
+              var tabarr=[];
+              for(var i in tablist){
+                tablist[i].font=tablist[i].sites.length+15;
+                tabarr.push(tablist[i]);          
+              }
+         
+              //  tabarr.sort(function(a,b){return b.font-a.font;});
+              
+              //display history view
+              $('#historylist').html('');
+              $('#historytemplate').mustache({tabs:tabarr}).appendTo('#historylist');
+                     
+             $(".domain").click(function(){
+                  $(this).siblings(".sites").toggle();
+             });
+              
+              
+              //get the google maps visits
+              var maplist=[];
+              for(var i in tabs){
+               if(tabs[i].url.match(/^https?:\/\/maps\.google\./)){
+                   var parsed=parseUri(tabs[i].url);
+                   console.log(parsed.queryKey.q)
+                   var q=parsed.queryKey.q;
+                   var image='http://maps.googleapis.com/maps/api/staticmap?center='+q+'&zoom=14&size=250x250&maptype=roadmap&sensor=false'
+                   maplist.push({url:tabs[i].url, image:image, q:q});
+               }
+              }
+               //display map view
+              $('#maplist').html('');
+              $('#maptemplate').mustache({maps:maplist}).appendTo('#maplist');
+                     
+             $(".domain").click(function(){
+                  $(this).siblings(".sites").toggle();
+             });
+              
+
           });
           
           
